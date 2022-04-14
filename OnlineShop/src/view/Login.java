56202e8c0f67c99c5b00e99bb21a81218b1f0e44
Daedalus <ernestojosh2@gmail.com>
2022-04-14 15:34:22 +0800

    database

--- OnlineShop/src/view/Login.java
+++ OnlineShop/src/view/Login.java
@@ -1,10 +1,15 @@
 package view;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.LoginController;
+import data.ProductManager;
+
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -13,74 +18,73 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 
-public class Login extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private ProductManager productManager = new ProductManager();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+
+	/**
+
+		*/
+	public Login() {
+
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new Empt
+
+		contentPane.setLayout(nul
+
+		
+		JPanel panel = new JPanel
+
+		
+		contentPane.add(panel);
+
+
+			anel_1.setBounds(355, 11, 349, 415);
+		contentPane.add(panel_1);
+
+			sernameTextBox = new JTextField();
+		usernameTextBox.setBounds(126, 148, 121, 20);
+		panel_1.add(usernameTextBox);
+		usernameTextBox.setColumns(10);
+
+		JButton btnNewButton = new JButton("Login");
+
+		btnNewButton.setBounds(139, 257, 89, 23);
+		panel_1.add(btnNewButton);
+
+		JLabel lblNewLabel = new JLabel("Username");
+		lblNewLabel.setBounds(38, 151, 78, 14);
+		panel_1.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(35, 205, 81, 14);
+		panel_1.add(lblNewLabel_1);
+
+		userPasswordTextBox = new JPasswordField();
+		userPasswordTextBox.setBounds(126, 202, 121, 20);
+		panel_1.add(userPasswordTextBox);
+
+		JLabel loginStatus = new JLabel("");
+		loginStatus.setBounds(38, 309, 137, 14);
+		panel_1.add(loginStatus);
+
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button Click");
+				String userName = usernameTextBox.getText();
+				String userPass = userPasswordTextBox.getText();
+				String status = loginController.login(userName, userPass);
+				loginStatus.setText(status);
+				if(e.getSource()==btnNewButton){
+					contentPane.setVisible(false);
+				HomeScreen myHomeScreen = new HomeScreen();
 				}
+				
 			}
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Login() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 720, 465);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(135, 206, 235));
-		panel.setBounds(0, 11, 355, 415);
-		contentPane.add(panel);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(135, 206, 235));
-		panel_1.setBounds(355, 11, 349, 415);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(126, 148, 121, 20);
-		panel_1.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnNewButton.setBounds(139, 257, 89, 23);
-		panel_1.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("Username");
-		lblNewLabel.setBounds(38, 151, 78, 14);
-		panel_1.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Password");
-		lblNewLabel_1.setBounds(35, 205, 81, 14);
-		panel_1.add(lblNewLabel_1);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(126, 202, 121, 20);
-		panel_1.add(passwordField);
-	}
 }
